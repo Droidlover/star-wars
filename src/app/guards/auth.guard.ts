058e9e688d660e6fd8a50e4cc3bcda2711519981
Droidlover <amanjain92@gmail.com>
2019-07-20 15:17:47 +0530

    asd

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,6 +1,6 @@
 import { StarWarsService } from './../services/star-wars.service';
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, Router,RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,13 +8,15 @@ import { Observable } from 'rxjs';
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private starWarsService: StarWarsService) { }
+  constructor(private starWarsService: StarWarsService,
+    private router: Router) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (!this.starWarsService.isLoggedIn()) {
       alert('User not logged in.');
+      this.router.navigate(['login']);
     };
     return this.starWarsService.isLoggedIn();
   }
